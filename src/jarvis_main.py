aaff52ecde7db25a07506b12c8675da561119c53
aryamansriram <aryaman.sriram@gmail.com>
2024-12-06 17:41:23 -0500

    Added stopping condition

--- src/jarvis_main.py
+++ src/jarvis_main.py
@@ -5,13 +5,15 @@ if __name__ == "__main__":
     messages = []
     while True:
         inp = input("You: ")
+        if inp == "exit":
+            break
         messages.append({"role": "user", "content": inp})
 
         stream = chat(model="llama3.2", messages=messages, stream=True)
-        out = ''
+        out = ""
         for chunk in stream:
             print(chunk["message"]["content"], end="", flush=True)
             out += chunk["message"]["content"]
-        
+
         messages.append({"role": "assistant", "content": out})
         print("\n")

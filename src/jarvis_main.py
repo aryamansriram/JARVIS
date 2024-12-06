7d596a94e9052c6bfc29a6fbd34404608b66f301
aryamansriram <aryaman.sriram@gmail.com>
2024-12-06 17:39:40 -0500

    Cleaned the code and settled on Ollama

--- src/jarvis_main.py
+++ src/jarvis_main.py
@@ -1,55 +1,17 @@
 from ollama import chat
-import time
-from transformers import pipeline,AutoModelForCausalLM
-
-def sample_dataset():
-    ls = [
-        {'role': 'user', 'content': 'How many countries are there in this world? Just tell me in one line'},
-        {'role': 'assistant', 'content': 'Tell me in 3 lines who Theodore Roosevelt was'},
-        {'role': 'user', 'content': 'Write me a essay about the film: Inglorious Bastards'},
-       
-    ]
-    return ls
 
 
-if __name__ == '__main__':
-    dset = sample_dataset()
-    times = []
-    MODE='coreml'
-    for example in dset:
-        #inp = input('You: ')
-        start = time.time()
-        if MODE == 'ollama':
-            stream = chat(
-                model='llama3.2',
-                messages=[example],
-                stream=True
-            )
-            print('Jarvis: ', end='', flush=True)
-            for chunk in stream:
-                print(chunk['message']['content'], end='', flush=True)
-            end = time.time()
-            times.append(end - start)
-            print('\n')
-    
-        elif MODE == 'mlx-hf':
-            
-            
-            pipe = pipeline("text-generation", model="mlx-community/Llama-3.2-3B-Instruct")
-            start  = time.time()
-            out = pipe([example])
-            end = time.time()
-            times.append(end - start)
-            print("OP: ",out)
+if __name__ == "__main__":
+    messages = []
+    while True:
+        inp = input("You: ")
+        messages.append({"role": "user", "content": inp})
+
+        stream = chat(model="llama3.2", messages=messages, stream=True)
+        out = ''
+        for chunk in stream:
+            print(chunk["message"]["content"], end="", flush=True)
+            out += chunk["message"]["content"]
         
-        elif MODE == 'coreml':
-            model = AutoModelForCausalLM.from_pretrained("andmev/Llama-3.2-3B-Instruct-CoreML")
-            # pipe = pipeline("text-generation", model="andmev/Llama-3.2-3B-Instruct-CoreML")
-            # start  = time.time()
-            # out = pipe([example])
-            # end = time.time()
-            # times.append(end - start)
-            # print("OP: ",out)
-
-    print(times)
-    print('Average time: ', sum(times) / len(times))
+        messages.append({"role": "assistant", "content": out})
+        print("\n")
